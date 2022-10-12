39c07aa05eb40015c416ca26bd68d38479f5e31d
Jim Kocsis <jim@wecancodeit.org>
2022-10-12 11:02:06 -0400

    Initial commit of sample app for MVC Education

---
+++ WebApplication1/Controllers/JimController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class JimController : Controller
+    {
+        // 
+        // GET: /Jim
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Jim/Welcome
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
